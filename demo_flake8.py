6ba4d361e4764cd9ec2f3af99a799081f081be64
吕静婷 <lvjingting@lvjingtingdeMacBook-Pro.local>
2023-02-13 14:39:37 +0100

    update

--- demo_flake8.py
+++ demo_flake8.py
@@ -1,7 +1,8 @@
+from pathlib import Path
 print(
     "hellor world hellor world hellor world hellor world hellor world hellor world hellor world hellor world hellor world"
 )
-from pathlib import Path
+
 
 package_dir = Path(__file__).parent.absolute()
 
